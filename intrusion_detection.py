38c8c67b4d88bb5120d0620b07cdfc30f84b5b1d
Sven <denhartog.sven@gmail.com>
2017-03-23 20:25:17 +0100

    fixed the preprocessing, added attack as feature, show normalized confusion maxtrix

--- intrusion_detection.py
+++ intrusion_detection.py
@@ -3,6 +3,7 @@ import numpy as np
 import sklearn as sk
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
 
 header = pd.read_table("kddcup.names.txt", header=None)
 att_types = pd.read_table("training_attack_types.txt", sep=" ", header=None)
@@ -17,11 +18,10 @@ def preprocess(dat):
     for i in range(0, len(att_types["attack"])):
         dat.loc[dat["attack"] == att_types.loc[i,].attack, "type"] = att_types.loc[i,].type
     dat.type = dat.type.fillna("unlisted")
-    dat.attack = dat.attack.astype('category')
 
     dat[dat.select_dtypes(include=['number']).columns] = preprocessing.scale(
         dat[dat.select_dtypes(include=['number']).columns])
-    dat = pd.get_dummies(dat, columns=['protocol_type', 'service', 'flag'])
+    dat = pd.get_dummies(dat, columns=['protocol_type', 'service', 'flag', 'attack'])
 
     return dat
 
@@ -30,16 +30,24 @@ tr = preprocess(tr_raw)
 test = preprocess(test_raw)  # actual test data (but labeled--don't cheat!)
 
 # not occurring in training set but occuring in test set so we just add it
-tr['service_icmp'] = 0
-test['service_red_i'] = 0
-test['service_urh_i'] = 0
+for i in np.setdiff1d(test.columns, tr.columns):
+    tr[i] = 0
+for i in np.setdiff1d(tr.columns, test.columns):
+    test[i] = 0
+test = test[tr.columns.tolist()]
 
 tr_labels = tr["type"].values
-tr_features = tr.drop(["type", "attack"], axis=1).values
+tr_features = tr.drop(["type"], axis=1).values
 
 test_labels = test["type"].values
-test_features = test.drop(["type", "attack"], axis=1).values
+test_features = test.drop(["type"], axis=1).values
 
-clf = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
+clf = RandomForestClassifier(n_estimators=20, max_depth=None, random_state=0)
 clf.fit(tr_features, tr_labels)
-print clf.score(test_features, test_labels)
+np.set_printoptions(suppress=True)
+print(clf.score(test_features, test_labels))
+types = tr['type'].unique()
+print(types)
+cm = confusion_matrix(test_labels, clf.predict(test_features), types)
+cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+print(cm)
